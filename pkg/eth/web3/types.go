3e92a5b96dd5237a4ec269f6a31afc965650168b
Brian McGee <brian@41north.dev>
2022-08-31 09:40:02 +0100

    feat: add tx web3 model

--- pkg/eth/web3/types.go
+++ pkg/eth/web3/types.go
@@ -3,6 +3,7 @@ package web3
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/ethereum/go-ethereum/common"
 	"math/big"
 	"regexp"
 
@@ -97,31 +98,117 @@ type Syncing struct {
 	Status    json.RawMessage
 }
 
-type Block struct {
-	Author           string `json:"author,omitempty"`
-	Miner            string `json:"miner,omitempty"`
-	Difficulty       string `json:"difficulty"`
-	TotalDifficulty  string `json:"totalDifficulty"`
-	GasLimit         string `json:"gasLimit"`
-	GasUsed          string `json:"gasUsed"`
+type AccessTuple struct {
+	Address     common.Address `json:"address"        gencodec:"required"`
+	StorageKeys []common.Hash  `json:"storageKeys"    gencodec:"required"`
+}
+
+type AccessList = []AccessTuple
+
+type Transaction struct {
+	// Common fields
 	Hash             string `json:"hash"`
-	MixHash          string `json:"mixHash"`
-	LogsBloom        string `json:"logsBloom"`
-	ExtraData        string `json:"extraData"`
 	Nonce            string `json:"nonce"`
-	Number           string `json:"number"`
-	ParentHash       string `json:"parentHash"`
-	ReceiptsRoot     string `json:"receiptsRoot"`
-	SealFields       string `json:"sealFields,omitempty"`
-	Sha3Uncles       string `json:"sha3Uncles"`
-	Size             string `json:"history,omitempty"`
-	StateRoot        string `json:"stateRoot"`
-	Timestamp        string `json:"timestamp"`
-	TransactionsRoot string `json:"transactionsRoot"`
+	BlockHash        string `json:"blockHash"`
+	BlockNumber      string `json:"blockNumber"`
+	TransactionIndex string `json:"transactionIndex"`
+	From             string `json:"from"`
+	To               string `json:"to,omitempty"`
+	Value            string `json:"value"`
+	Gas              string `json:"gas"`
+	Input            string `json:"input"`
+	Data             string `json:"data,omitempty"`
+	V                string `json:"v"`
+	R                string `json:"r"`
+	S                string `json:"s"`
 
-	// todo add transactions and uncles
+	// EIP-155
+	ChainID string `json:"chainId,omitempty"`
 
+	// legacy tx pricing
+	GasPrice string `json:"gasPrice,omitempty"`
+
+	// EIP-1559
+	Type                 string `json:"type,omitempty"`
+	MaxFeePerGas         string `json:"MaxFeePerGas,omitempty"`
+	MaxPriorityFeePerGas string `json:"maxPriorityFeePerGas,omitempty"`
+
+	// EIP-2930
+	AccessList AccessList `json:"accessList,omitempty"`
+}
+
+type Block struct {
+	Author          string   `json:"author,omitempty"`
+	Miner           string   `json:"miner,omitempty"`
+	Difficulty      string   `json:"difficulty"`
+	TotalDifficulty string   `json:"totalDifficulty"`
+	GasLimit        string   `json:"gasLimit"`
+	GasUsed         string   `json:"gasUsed"`
+	Hash            string   `json:"hash"`
+	MixHash         string   `json:"mixHash"`
+	LogsBloom       string   `json:"logsBloom"`
+	ExtraData       string   `json:"extraData"`
+	Nonce           string   `json:"nonce"`
+	Number          string   `json:"number"`
+	ParentHash      string   `json:"parentHash"`
+	ReceiptsRoot    string   `json:"receiptsRoot"`
+	SealFields      string   `json:"sealFields,omitempty"`
+	Sha3Uncles      string   `json:"sha3Uncles"`
+	Size            string   `json:"history,omitempty"`
+	StateRoot       string   `json:"stateRoot"`
+	Timestamp       string   `json:"timestamp"`
+	Uncles          []string `json:"uncles"`
+
+	TransactionsRoot string            `json:"transactionsRoot"`
+	Transactions     []json.RawMessage `json:"transactions"` // list of hashes or a list of transactions objects
+
+	TransactionHashes  []string
+	TransactionObjects []Transaction
+
+	// EIP-1559
 	BaseFeePerGas string `json:"baseFeePerGas,omitempty"`
+}
+
+func (b *Block) UnmarshalTransactions() error {
+	count := len(b.Transactions)
+
+	if count == 0 || len(b.TransactionHashes) > 0 || len(b.TransactionObjects) > 0 {
+		// no transactions or we have already unmarshalled them
+		return nil
+	}
+
+	// check the first character of the first entry
+	switch b.Transactions[0][0] {
+	case '"':
+		// we assume an array of hashes
+		var result []string
+		for _, item := range b.Transactions {
+			var hash string
+			if err := json.Unmarshal(item, &hash); err != nil {
+				return err
+			}
+			result = append(result, hash)
+		}
+		// cache the result
+		b.TransactionHashes = result
+	case '{':
+		// we assume an array of transaction objects
+		// we assume an array of hashes
+		var result []Transaction
+		for _, item := range b.Transactions {
+			var tx Transaction
+			if err := json.Unmarshal(item, &tx); err != nil {
+				return err
+			}
+			result = append(result, tx)
+		}
+		// cache the result
+		b.TransactionObjects = result
+	default:
+		return errors.Errorf("unexpected format encountered: %s", b.Transactions[0])
+	}
+
+	return nil
 }
 
 type NewHead struct {
